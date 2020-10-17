b09abb5cbd12106959f662554c255b8f039891a2
architnagpal001 <63536534+architnagpal001@users.noreply.github.com>
2020-10-17 21:14:01 +0530

    Update db.php
    condition

--- db.php
+++ db.php
@@ -2,5 +2,5 @@
  $link = mysqli_connect("localhost", "xyz", "xyz@321", "xyz");
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . mysqli_connect_error()); //condition
 }?>
